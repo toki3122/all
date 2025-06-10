4f9c6c272265b986f0d4598da767a58fa9dd7ac6
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-06-10 22:10:50 +0600

    Update randomnumber.cpp

--- randomnumber.cpp
+++ randomnumber.cpp
@@ -5,8 +5,7 @@ int main()
 {
     for(int i=1;i<=5;i++)
     {
-        //rand function ta random number generate korte pare
-        int random=rand()%5+1;//eta korlam jate amake shob shomoy 1 theke 5 value return kore
+        int random=rand()%5+1;
         cout<<"random number= "<<random<<endl;
     }
     getch();
